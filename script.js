aa3fafde8ba8b9be6c730d7800e2ed24ae4c7ccf
aleksfilmore <iamalexfilip@gmail.com>
2025-06-19 21:09:54 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('scroll', () => {
-  document.querySelectorAll('.fade-on-scroll').forEach(el => {
-    const rect = el.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      el.classList.add('visible');
-    }
-  });
-});
