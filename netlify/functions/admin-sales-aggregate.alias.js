98e8852913c55db20e9fc7d1bfa139b7720c68b3
aleksfilmore <iamalexfilip@gmail.com>
2025-08-26 22:17:14 +0200

    867

--- netlify/functions/admin-sales-aggregate.alias.js
+++ netlify/functions/admin-sales-aggregate.alias.js
@@ -1,3 +1,4 @@
-// Alias to forward older or alternate function names to the canonical implementation
+// DEPRECATED: This file name contains a dot and causes Netlify to create an invalid function name.
+// It has been replaced by `admin-sales-aggregate-alias.js`. Keep this file only temporarily.
 const mod = require('./admin-sales-aggregate');
 exports.handler = mod.handler;
