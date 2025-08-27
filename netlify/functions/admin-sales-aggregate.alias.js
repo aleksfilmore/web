5a462de98d11e106830683e04e31160508d9cf2f
aleksfilmore <iamalexfilip@gmail.com>
2025-08-27 06:07:17 +0200

    fix(functions): remove invalid admin-sales-aggregate.alias.js and add valid alias

--- netlify/functions/admin-sales-aggregate.alias.js
+++
@@ -1,4 +0,0 @@
-// DEPRECATED: This file name contains a dot and causes Netlify to create an invalid function name.
-// It has been replaced by `admin-sales-aggregate-alias.js`. Keep this file only temporarily.
-const mod = require('./admin-sales-aggregate');
-exports.handler = mod.handler;
