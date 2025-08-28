728e0a24c96068e6d086182a3a45420aa2499dde
aleksfilmore <iamalexfilip@gmail.com>
2025-08-28 16:18:59 +0200

    fix(functions): remove invalid function filename admin-sales-aggregate.alias.js

--- netlify/functions/admin-sales-aggregate.alias.js
+++
@@ -1,7 +0,0 @@
-// Alias to forward older or alternate function names to the canonical implementation
-// Alias wrapper for admin-sales-aggregate (renamed to avoid invalid characters)
-const mod = require('./admin-sales-aggregate');
-
-exports.handler = async (event, context) => {
-	return mod.handler(event, context);
-};
