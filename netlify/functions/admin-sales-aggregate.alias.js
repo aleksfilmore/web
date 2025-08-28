8053a287a9ee3581546b6991ac2f7fe8dc687aad
aleksfilmore <iamalexfilip@gmail.com>
2025-08-28 16:16:28 +0200

    ADMIN 2

--- netlify/functions/admin-sales-aggregate.alias.js
+++ netlify/functions/admin-sales-aggregate.alias.js
@@ -1,3 +1,7 @@
 // Alias to forward older or alternate function names to the canonical implementation
+// Alias wrapper for admin-sales-aggregate (renamed to avoid invalid characters)
 const mod = require('./admin-sales-aggregate');
-exports.handler = mod.handler;
+
+exports.handler = async (event, context) => {
+	return mod.handler(event, context);
+};
